ff2e4f8c23aa82024147b2a7fd8fa61040d8676e
canh2808 <phamngoccanh.hmm@gmail.com>
2023-09-25 00:57:37 +0700

    call and send message

--- app/src/main/java/com/example/fastcar/Fragment/HoTro_Fragment.java
+++ app/src/main/java/com/example/fastcar/Fragment/HoTro_Fragment.java
@@ -1,11 +1,16 @@
 package com.example.fastcar.Fragment;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.text.Html;
@@ -14,12 +19,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.fastcar.R;
 
 public class HoTro_Fragment extends Fragment {
     TextView tv_hotline_html;
     Button btn_call, btn_send_message;
+    String phone_number = "022266688822";
+    private static final int REQUEST_CALL_PERMISSION = 123;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -42,7 +50,12 @@ public class HoTro_Fragment extends Fragment {
         btn_call.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if (ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                    requestCall();
+                } else {
+                    // chưa có quyền, yêu cầu quyền CALL_PHONE từ người dùng
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.CALL_PHONE}, REQUEST_CALL_PERMISSION);
+                }
             }
         });
 
@@ -50,7 +63,7 @@ public class HoTro_Fragment extends Fragment {
         btn_send_message.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                request_message();
             }
         });
     }
@@ -59,5 +72,42 @@ public class HoTro_Fragment extends Fragment {
         tv_hotline_html = view.findViewById(R.id.tv_hotline_html);
         btn_call = view.findViewById(R.id.btn_call_to_hotline);
         btn_send_message = view.findViewById(R.id.btn_send_message_to_hotline);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CALL_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // đã cấp quyền
+                requestCall();
+            } else {
+                Toast.makeText(getContext(), "Không có quyền thực hiện cuộc gọi", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void requestCall() {
+        String phoneNumberWithDialer = "tel:" + phone_number;
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse(phoneNumberWithDialer));
+
+        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Toast.makeText(getContext(), "Không có ứng dụng gọi điện thoại", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void request_message() {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("sms:" + phone_number));
+
+        // Kiểm tra xem có ứng dụng tin nhắn nào để xử lý Intent này
+        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Toast.makeText(getContext(), "Không có ứng dụng tin nhắn", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
